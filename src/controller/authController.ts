4be703284673177879622190b7a7dab783f0ae34
fitsumhelina <dev.fitsum@gmail.com>
2025-07-11 13:25:56 +0300

    feat(auth): implement user login functionality with password validation and JWT token generation

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -35,3 +35,27 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
     }
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+        if (!user) {
+            return res.status(400).json({ message: "user not found" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ message: "Login successful", token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+
+    
+}
